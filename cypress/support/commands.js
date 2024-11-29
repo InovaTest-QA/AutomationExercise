7b54b5133bd5297941d4d7cdedc930382069660b
Gustavo Henrique <gustavohenrique@pactosolucoes.com.br>
2024-11-28 23:08:36 -0300

    Adiciona o comando personalizado loginViaUI

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,7 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('loginViaUI', () => {
+  cy.visit('https://automationexercise.com/login');
+  cy.get('input[data-qa="login-email"]').should('be.visible').type('teste01@gmail.com');
+  cy.get('input[data-qa="login-password"]').should('be.visible').type('teste01');
+  cy.get('button[type="submit"]').should('be.visible').first().click();
+  cy.url().should('not.include', '/login');
+});
